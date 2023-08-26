faf4535a556e24b0edf8bac70c2400de45523861
Abhishek Mishra <abhishekmishra45658@gmail.com>
2023-08-27 03:00:31 +0530

    Updates

---
+++ precache-manifest.7d6477696331409ff0e7826a8a749988.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c7beab66d5aec8363fe1",
+    "url": "/reactnew/static/css/main.ccb594ab.chunk.css"
+  },
+  {
+    "revision": "c7beab66d5aec8363fe1",
+    "url": "/reactnew/static/js/main.c7beab66.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/reactnew/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/reactnew/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "3499793f692d5d0831d7",
+    "url": "/reactnew/static/js/runtime~main.3499793f.js"
+  },
+  {
+    "revision": "10aca74697984563b8ed1941679a1571",
+    "url": "/reactnew/index.html"
+  }
+];
